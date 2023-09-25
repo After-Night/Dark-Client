1e5624f76f8307f9ddd90c343c82833d17cdb871
AfterNight <146012800+After-Night@users.noreply.github.com>
2023-09-25 12:48:33 -0500

    Create Blacklist.lua

---
+++ Blacklist.lua
@@ -0,0 +1,6 @@
+local plr = game:GetService("Players").LocalPlayer
+if plr.PlayerName == "TheTableAlwaysWin" or "testingaccount342187" or "efewfewf28" or "MoonTable0" or "NightTable0" or "TableNotify" or "TableNotify505" or "xxSMOKExx_1" then
+  plr:Kick("You have been blacklisted from this client. [Remaining ban duration: Infinite]")
+end
+
+print("File executed successfully")
